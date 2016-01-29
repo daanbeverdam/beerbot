b278065103eee860688bf8f240206166a8d7cf81
Daan Beverdam <daanbeverdam@gmail.com>
2016-01-29 19:38:37 +0100

    deleted models that were not used

--- models/beer_meal.py
+++
@@ -1,8 +0,0 @@
-from peewee import *
-from base import Base
-
-class BeerMeal(Base):
-    """The BeerMeal class epresents a beer_meal connection in the database."""
-    id = PrimaryKeyField()
-    beer_id = IntegerField()
-    meal_id = IntegerField()
--- models/meal.py
+++
@@ -1,7 +0,0 @@
-from peewee import *
-from base import Base
-
-class Meal(Base):
-    """The Meal class epresents a meal entry in the database."""
-    id = PrimaryKeyField()
-    name = CharField()
